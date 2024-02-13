099a6b17f7196091569a5ae183b8d3b6849981d9
Abhimanyu2808 <abhimanyupatil2808@gmail.com>
2024-02-13 21:58:01 +0530

    add

--- ec2.tf
+++ ec2.tf
@@ -2,6 +2,34 @@ provider "aws" {
   region = "ap-south-1"  
 }
 
+
+resource "aws_security_group" "instance_sg" {
+  name        = "my-instance-sg"
+  description = "Security group for the example EC2 instance"
+  
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+  
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"  
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "my-instance" {
   ami           = "ami-03f4878755434977f"         
   instance_type = "t2.micro"             
