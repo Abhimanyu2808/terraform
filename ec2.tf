5360342362dca0de11f6379b737e1397b58675be
Abhimanyu2808 <abhimanyupatil2808@gmail.com>
2024-02-13 20:10:17 +0530

    add

--- ec2.tf
+++ ec2.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "my-instance" {
   ami           = "ami-03f4878755434977f" 
   instance_type = "t2.micro"
   key_name      = "new-key"
-  security_group_id = ["sg-038f5dbc2b42e1eaa"] 
+  security_groups = ["sg-038f5dbc2b42e1eaa"] 
 
   tags = {
   
