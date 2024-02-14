8720989d0865bc1753976d163281d82119130854
Abhimanyu2808 <abhimanyupatil2808@gmail.com>
2024-02-14 14:16:05 +0530

    add

--- vpc.tf
+++ vpc.tf
@@ -13,7 +13,8 @@ resource "aws_vpc" "my_vpc" {
 resource "aws_subnet" "private_subnet" {
     vpc_id     = aws_vpc.my_vpc.id
     cidr_block = var.private_subnet_cidr
-
+    availability_zone = ap-south-1a
+    availability_zone_id = aps1-az1
     tags = {
         env = var.env
         Name = "${var.project}-private-subnet"
@@ -23,6 +24,8 @@ resource "aws_subnet" "private_subnet" {
 resource "aws_subnet" "public_subnet" {
     vpc_id     = aws_vpc.my_vpc.id
     cidr_block = var.public_subnet_cidr
+    availability_zone = ap-south-1a
+    availability_zone_id = aps1-az1
     map_public_ip_on_launch = true
     tags = {
         env = var.env
